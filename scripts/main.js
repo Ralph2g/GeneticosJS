db9bf17c88906f3701dba1e6a554aa3407615ab8
Rafael Garcia Garcia <Ralph2g@github.com>
2018-11-06 00:47:26 -0600

    Update scripts/main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,33 @@ var poblacion = [];
 var poblacionAlInicio = [];
 var i,j;
 var sumaTotal = 0;
-var fitnessAcumulado = 0;
 var datosGeneraciones = [];
-
+//restricciones
+var r1 = []; //a,b,c,d, <=>, restriccion
+var r2 = [];
+var r3 = [];
+var r4 = [];
+var r5 = [];
+//
+var rx=[];
+var ry=[];
+var rz=[];
+var rw=[];
+//Rangos de las variables 
+var rangoX = [0,0];// rango de las variables
+var rangoY = [0,0];// rango de las variables
+var rangoZ = [0,0];// rango de las variables
+var rangoW = [0,0];// rango de las variables
 //Variables iniciales
-var pc = 0.70;
-var genes = 30;
-var pm = 0.0555;
+var genes = 30; // numero de bits que utlizaremos
+var mjX = 0;// numero de cromosomas de cada variable
+var mjY = 0;// numero de cromosomas de cada variable
+var mjZ = 0;// numero de cromosomas de cada variable
+var mjW = 0;// numero de cromosomas de cada variable
+
 var poblacionInicial = 50;
 var generaciones = 30;
-
+var x = "poblacion"
 $(document).ready(function(){
     $('form').submit(function(){
         return false;
@@ -21,35 +38,38 @@ $(document).ready(function(){
 
 /*Funcion que genera el algoritmo genetico*/
 function evoluciona(){
-    $('.loading').removeClass('hide');
-    poblacionInicial 	= $('#poblacion').val();
-    pc 					= $('#cruzamiento').val();
-    pm 					= $('#mutacion').val();
-    generaciones 		= $('#generaciones').val();
+
+    poblacionInicial 	= ($('#poblacion').val());
+    generaciones 		= ($('#generaciones').val());
+    
+
+    //guardamos las restricciones
+    r1 = restricciones(1);
+    r2 = restricciones(2);
+    r3 = restricciones(3);
+    r4 = restricciones(4);
+    r5 = restricciones(5);
+    
+    //creamos los rangos de cada una de las varibles}
+    console.log("r1");
+    console.log(r1);
+    console.log("r2");
+    console.log(r2);
+    rangos()
+
+    // obtenemos los tamaños de cromosomas de cada variable    
+    mjX = numMJ(rangoX[0],rangoX[1],1);//enviamos limite inf, limite sup, precision de bit    
+    mjY = numMJ(rangoY[0],rangoY[1],1);//enviamos limite inf, limite sup, precision de bit
+    mjZ = numMJ(rangoZ[0],rangoZ[1],4);//enviamos limite inf, limite sup, precision de bit
+    mjW = numMJ(rangoW[0],rangoW[1],1);//enviamos limite inf, limite sup, precision de bit
+        
+    genes = mjX + mjY + mjZ + mjW; // definimos el tamaño del vector
+    
     iniciaPoblacion();
-    evaluaPoblacion();
-    datosGeneraciones 	= [];
-    console.table(poblacionAlInicio);
-    $('.res-ag').removeClass('toHide');
-    graficaPastel('#genoma-inicio', poblacionAlInicio, 'Distribución al inicio');
-    imprimeTablaInicial();
-    for(var k=0;k<generaciones;k++){
-        evaluaPoblacion();
-        var padre = {};
-        padre.A = seleccionPadres();
-        padre.B = seleccionPadres();
-        console.log('Padres:');
-        console.log(padre);
-        var hijos = cruzamiento(padre);
-        var mutados = mutacion(hijos);
-        sustituyePoblacion(mutados);
-        evaluaPoblacion();
-        datosGeneraciones.push(obtieneDatosGeneracion());
-    }
-    console.table(poblacion);
-    imprimeTablaFinal();
-    graficaGenoma(datosGeneraciones);
-    graficaPastel('#genoma-final', poblacion, 'Distribución al final');
+    console.log(poblacion);
+
+
+    
 }
 
 /*Funcion carga una población inicial para evolucionar*/
@@ -57,204 +77,232 @@ function iniciaPoblacion(){
     console.log('--- Poblacion Inicial ---');
     poblacion = [];
     for(i=0;i<poblacionInicial;i++){
+        
         var objetoInicial = {
-            cromosomaArray:[]
+            cromosomaindice:0,
+            cromosomaArray:[],
+            cromosomaValor:0,
+            cromosomaArrayX:[],
+            cromosomaArrayY:[],
+            cromosomaArrayZ:[],
+            cromosomaArrayW:[],
+            cromosomaValorX:0,
+            cromosomaValorY:0,
+            cromosomaValorZ:0,
+            cromosomaValorW:0,
+            
         };
-        objetoInicial.cromosomaindice = i;
-        for(j=0;j<genes;j++){
-            var aleatorio = numeroAleatorio();
-            objetoInicial.cromosomaArray.push(aleatorio);
-        }
-        poblacion.push(objetoInicial);
-    }
+        objetoInicial.cromosomaindice = i+1;
+        do {
+            var valido = false;
+            for(j=0;j<genes;j++){
+                var aleatorio = numeroAleatorio();
+                objetoInicial.cromosomaArray.push(aleatorio);
+            }
+            objetoInicial.cromosomaValor = (parseInt(objetoInicial.cromosomaArray.join(""),2));// convertimos el binario de la cadena a decimal
+            // Cortamos los arreglos en los tamaños correspondientes
+            objetoInicial.cromosomaArrayX = objetoInicial.cromosomaArray.slice(0,mjX);//PRIMERO QUE IMPRIME, UNO ANTES DEL QUE IMPRIME
+            objetoInicial.cromosomaArrayY = objetoInicial.cromosomaArray.slice(mjX,mjY+mjX);
+            objetoInicial.cromosomaArrayZ = objetoInicial.cromosomaArray.slice(mjX+mjY,mjZ+mjX+mjY);
+            objetoInicial.cromosomaArrayW = objetoInicial.cromosomaArray.slice(mjZ+mjX+mjY,mjZ+mjX+mjY+mjW);
+            
+            objetoInicial.cromosomaValorX = valArreglo(objetoInicial.cromosomaArrayX)
+            objetoInicial.cromosomaValorY = valArreglo(objetoInicial.cromosomaArrayY)
+            objetoInicial.cromosomaValorZ = valArreglo(objetoInicial.cromosomaArrayZ)
+            objetoInicial.cromosomaValorW = valArreglo(objetoInicial.cromosomaArrayW)
+            
+            
+            
+            if (evaluaCromosoma(objetoInicial.cromosomaValorX,objetoInicial.cromosomaValorY,objetoInicial.cromosomaArrayZ,objetoInicial.cromosomaArrayW))
+                valido = true;
+        
+        }while (valido == false)// fin del while
+        
+
+        
+        
+        
+        poblacion.push(objetoInicial);//incluimos a el individuo en nuestra poblacion
+        
+        
+    }// fin del for que genera poblacion individual
     poblacionAlInicio = poblacion; //Guarda la poblacion al inicio para comparar.
-    console.log(poblacion);
+    
 }
-/*Evalua la población asignado su calificacion(Fitness)*/
-function evaluaPoblacion(){
-    console.log('--- Evalua Poblacion ---');
-    sumaTotal = 0;
-    for(i=0;i<poblacion.length;i++){
-        poblacion[i].cromosomaBin = poblacion[i].cromosomaArray.join('');
-        poblacion[i].cromosomaTotal = ((parseInt(poblacion[i].cromosomaBin, 2))/2^30);
-        poblacion[i].fitness = fitness(poblacion[i].cromosomaBin);
-        sumaTotal+=poblacion[i].fitness;
+
+//guarda las Restricciones en arreglos: giTnoy7n
+function restricciones(n){
+    var r = [0,0,0,0,"",0]
+    var letra = ["a","b","c","d","s","cons"]
+    if (n == 1 ){
+        for (var i = 0; i < 6; i++){
+            if (i != 4) 
+                r[i] = ( $('#r1'+letra[i]+'').val() );
+            else
+                r[i] = $('#r1'+letra[i]+'').val();
+                
+        }
     }
-    generaProbSeleccion();
-    console.log(poblacion);
-}
-
-/*F(x) Funcion que  obtiene la calificacion basado en el numero de "1's" de la cadena*/
-function fitness(valor){
-    // devuelve la cantidad de "1" que contiene el cromosoma
-    unos = (valor.split("1").length-1)/30;
-    //return ( (1-Math.pow((11/2)*unos-(7/2),2))*(cos((11/2)*unos-(7/2))+1)+2   );
-    return (Math.pow(unos,7));
-}
-
-//Probabilidad de selección
-function generaProbSeleccion(){
-    console.log('--- Genera Probabilidad de seleccion ---');
-    $.each(poblacion,function(i,item){
-        var pselect = item.fitness/sumaTotal;
-        poblacion[i].probabilidad = pselect;
-    });
-}
-
-/*Funcion que obtiene el fitness minimo, fitness maximo y fitness promedio asi como su indice en el cromosma */
-function obtieneDatosGeneracion(){
-    var menor=30,mayor= 0,totPromedio= 0,imayor= 0, imenor=0;
-    for(i=0;i<poblacion.length;i++){
-        if(poblacion[i].fitness<=menor){
-            menor=poblacion[i].fitness;
-            imenor=poblacion[i].cromosomaindice;
+    if (n == 2) {
+        for (var i = 0; i < 6; i++){
+            if (i != 4)
+            r[i] = ( $('#r2'+letra[i]+'').val() );
+            else
+            r[i] = $('#r2'+letra[i]+'').val();
         }
-        if(poblacion[i].fitness>mayor){
-            mayor=poblacion[i].fitness;
-            imayor=poblacion[i].cromosomaindice;
-        }
-        totPromedio+=poblacion[i].fitness;
     }
-    return {menor:menor,mayor:mayor,promedio:(totPromedio/poblacion.length),imayor:imayor,imenor:imenor};
-}
-
-/*Imprime la poblacion inicial*/
-function imprimeTablaInicial(){
-    $('#resultados-orig tbody').html('');
-    $('.results').html('');
-    var html = '<tr><td> </td><td>--------Tabla Inicial--------</td><td> </td><td> </td><td> </td></tr>';
-    $('#resultados-orig tbody').append(html);
-    fitnessAcumulado = 0;
-    $.each(poblacionAlInicio,function(i,item){
-        fitnessAcumulado+=item.fitness;
-        var html = '<tr><td>'+i+'</td><td>'+item.cromosomaBin+'</td><td>'+item.fitness+'</td><td>'+item.probabilidad+'</td><td>'+fitnessAcumulado+'</td></tr>';
-        $('#resultados-orig tbody').append(html);
-    });
-    var html = '<tr><td> </td><td> Suma </td><td>'+fitnessAcumulado+'</td><td> </td><td> </td></tr><tr><td></td><td></td><td></td><td></td><td></td></tr>';
-    $('#resultados-orig tbody').append(html);
-}
-
-/*Imprime la poblacion final*/
-function imprimeTablaFinal(){
-    $('#resultados tbody').html('');
-    $('.results').html('');
-    $('.loading').addClass('hide');
-    var html = '<tr><td> </td><td>---------Tabla Final---------</td><td> </td><td> </td><td> </td></tr>';
-    $('#resultados tbody').append(html);
-
-    fitnessAcumulado = 0;
-    $.each(poblacion,function(i,item){
-        fitnessAcumulado+=item.fitness;
-        var html = '<tr><td>'+i+'</td><td>'+item.cromosomaBin+'</td><td>'+item.fitness+'</td><td>'+item.probabilidad+'</td><td>'+fitnessAcumulado+'</td></tr>';
-        $('#resultados tbody').append(html);
-    });
-    var html = '<tr><td> </td><td> Suma </td><td>'+fitnessAcumulado+'</td><td> </td><td> </td></tr>';
-    $('#resultados tbody').append(html);
-
-     console.log(datosGeneraciones[poblacionInicial-1]);
-     console.log(poblacionInicial-1);
-     $('.results').append('<p>Fitness Mayor (Genoma '+datosGeneraciones[generaciones-1].imayor+') = '+datosGeneraciones[generaciones-1].mayor+'</p>');
-     $('.results').append('<p>Fitness Menor (Genoma '+datosGeneraciones[generaciones-1].imenor+') = '+datosGeneraciones[generaciones-1].menor+'</p>');
-     $('.results').append('<p>Mejor Genoma = '+poblacion[datosGeneraciones[generaciones-1].imayor].cromosomaBin+'</p>');
-
-}
-
-/*Selecciona los padres para generar una nueva generacion*/
-function seleccionPadres(){
-    var r = Math.random();
-    var c = r*sumaTotal;
-    var Ca = 0;
-    var statusRetorno = false;
-    var seleccionado;
-    $.each(poblacion,function(i,item){
-        Ca+=item.fitness;
-        if((Ca > c) && (statusRetorno == false)){
-            seleccionado = item;
-            statusRetorno = true;
+    if (n == 3) {
+        for (var i = 0; i < 6; i++){
+            if (i != 4)
+            r[i] = ( $('#r3'+letra[i]+'').val() );
+            else
+            r[i] = $('#r3'+letra[i]+'').val();
+        } 
+    }
+    if (n == 4) {
+        for (var i = 0; i < 6; i++){
+            if (i != 4)
+            r[i] = ( $('#r4'+letra[i]+'').val() );
+            else
+            r[i] = $('#r4'+letra[i]+'').val();
         }
-    });
-    return seleccionado;
+        
+    }
+    if (n == 5) {
+        for (var i = 0; i < 6; i++){
+            if (i != 4)
+            r[i] = ($('#r5'+letra[i]+'').val() );
+            r[i] = $('#r5'+letra[i]+'').val();
+        }
+
+    }
+    return r;
 }
 
-/*Funcion que determina el cruzamiento*/
-function determinaCruzamiento(){
-    var d = Math.random();
-    if(d<pc){
+/*Ahora si es rangos */
+function rangos(){
+    //var rx=[]; objetoInicial.cromosomaArray.push(aleatorio);
+    /* cualquiernumero/!=0 */
+    if (restriccionValida(r1))
+        dividirValores(r1);
+    
+    if (restriccionValida(r2))
+        dividirValores(r2);
+        
+    if (restriccionValida(r3)) 
+        dividirValores(r3);
+        
+    if (restriccionValida(r4))
+        dividirValores(r4);
+        
+    if (restriccionValida(r5)) 
+        dividirValores(r5);
+    
+    
+    rangoX = [0,infinito(Math.max(...rx))];
+
+    rangoY = [0,infinito(Math.max(...ry))];
+
+    rangoZ = [0,infinito(Math.max(...rz))];
+
+    rangoW = [0,infinito(Math.max(...rw))];
+    console.log(typeof(rangoW[1]));
+
+}
+
+function restriccionValida(res){
+    if (res[0] == 0 && res[1] == 0 && res[2] == 0 && res[3] == 0){
+        return false;
+    }else {
         return true;
     }
-    else{
-        return false;
+}
+
+function dividirValores(res){
+    console.log("Entre ala funcion");
+    if(res[0] != 0)
+        rx.push(res[5]/res[0]);
+    if(res[1] != 0)
+        ry.push(res[5]/res[1]);
+    if(res[2] != 0)
+        rz.push(res[5]/res[2]);
+    if(res[3] != 0)
+        rw.push(res[5]/res[3]);
+        
+        console.log(res);
+        return 
+}
+function infinito(number){
+    
+    if (isFinite(number)) {
+        
+        return number;
+    }
+    else 
+        return 0;
+}
+/*Funcion que saca el valor MJ de las variables */
+function numMJ(aj,bj,n)
+{
+    var resultado;
+    if (aj == 0 && bj == 0) 
+        return resultado = 0;
+    else {
+    resultado = Math.ceil( Math.log((bj-aj)*( Math.pow(10,n) ))/Math.log(2));//valor techo
+    return resultado;
     }
 }
-
-/*Funcion que realiza el cruzamiento obteniendo los descendientes*/
-function cruzamiento(padre){
-    console.log('--- Genera Cruzamiento ---');
-    var hijo = {};
-    if(determinaCruzamiento()){
-        console.log('Van a cruzarse');
-        var l = genes;
-        var x = Math.round(Math.random()*(4-2)+parseInt(0));
-        console.log('Habrá cruze en: '+x);
-        hijo.A = {cromosomaBin:padre.A.cromosomaBin.substring(0, x)+padre.B.cromosomaBin.substring(x,l), cromosomaindice:padre.A.cromosomaindice};
-        hijo.B = {cromosomaBin:padre.B.cromosomaBin.substring(0, x)+padre.A.cromosomaBin.substring(x,l), cromosomaindice:padre.B.cromosomaindice};
-        hijo.A.cromosomaTotal = parseInt(hijo.A.cromosomaBin, 2);
-        hijo.B.cromosomaTotal = parseInt(hijo.B.cromosomaBin, 2);
-    }else{
-        console.log('No van a cruzarse');
-        hijo.A = padre.A;
-        hijo.B = padre.B;
-    }
-    console.log(hijo);
-    return hijo;
-}
-
-/*Funcion que genera la mutacion de la cadena en base al porcentaje de mutacion dado*/
-function mutacion(hijo){
-    var q;
-    var hijoA = [];
-    var hijoB = [];
-
-    for(i=0;i<genes;i++){
-        q = Math.random();
-        if(q<pm){
-            console.log('Habrá mutacion en: '+i);
-            if(hijo.A.cromosomaBin[i] == '0'){hijoA.push(1);}else{hijoA.push(0);}
-            if(hijo.B.cromosomaBin[i] == '0'){hijoB.push(1);}else{hijoB.push(0);}
-        }
-        else{
-            hijoA.push(hijo.A.cromosomaBin[i]);
-            hijoB.push(hijo.B.cromosomaBin[i]);
-        }
-    }
-    var nuevoHijo = {
-        A:{
-            cromosomaindice:hijo.A.cromosomaindice,
-            cromosomaArray:hijoA,
-            cromosomaBin: hijoA.join(''),
-            cromosomaTotal:parseInt(hijoA.join(''), 2)
-        },
-        B:{
-            cromosomaindice:hijo.B.cromosomaindice,
-            cromosomaArray:hijoB,
-            cromosomaBin: hijoB.join(''),
-            cromosomaTotal:parseInt(hijoB.join(''), 2)
-        }
-    };
-    console.log(nuevoHijo);
-    return nuevoHijo;
-}
-
-/*Sustiye los descendientes en la poblacion*/
-function sustituyePoblacion(nuevos){
-    poblacion[nuevos.A.cromosomaindice] = nuevos.A;
-    poblacion[nuevos.B.cromosomaindice] = nuevos.B;
-    console.log('--- Insertados en la poblacion ---');
-    //console.table(poblacion);
-}
-
 //Genera un número aleatorio.
 function numeroAleatorio(){
     var numero = Math.round(Math.random());
     return numero;
 }
+
+
+/*Funcion de valor para Xi*/
+function numXi()
+{
+    var aj;
+    var decimal;
+    var bj;
+    var mj;
+    var Xn;
+    Xn=aj+decimal( (bj-aj)/((Math.pow(2,mj))-1) );
+    return Xn;
+}
+function valArreglo(arreglo){
+    var numero = (parseInt(arreglo.join(""),2));
+    if(isNaN(numero))
+        return null;
+    else 
+        return numero
+}
+
+function evaluaCromosoma(cromoX,cromoY,cromoZ,cromoW){
+
+    //vamos a utilizar las restricciones gg
+    return true;
+    
+    
+}
+
+
+
+
+// imprimimos que si se guardan los valores de las restricciones en los arreglos 
+/*
+    for (var i = 0; i < r1.length; i++)
+    document.writeln(r1[i]+"<br>")
+    
+    for (var i = 0; i < r2.length; i++)
+    document.writeln(r2[i]+"<br>")
+    
+    for (var i = 0; i < r3.length; i++)
+    document.writeln(r3[i]+"<br>")
+    
+    for (var i = 0; i < r4.length; i++)
+    document.writeln(r4[i]+"<br>")
+    
+    for (var i = 0; i < r5.length; i++)
+    document.writeln(r5[i]+"<br>")
+    */
